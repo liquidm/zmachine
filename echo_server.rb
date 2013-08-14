94c52f7862d7ca24293e13f91bcf84c22b6d7bc8
Benedikt Böhm <bb@xnull.de>
2013-08-14 16:47:28 +0200

    remove broken trap handler

--- echo_server.rb
+++ echo_server.rb
@@ -11,10 +11,6 @@ class EchoServer < ZMachine::Connection
 end
 
 ZMachine.run do
-  # hit Control + C to stop
-  Signal.trap("INT")  { ZMachine.stop }
-  Signal.trap("TERM") { ZMachine.stop }
-
   ZMachine.start_server("0.0.0.0", 10000, EchoServer)
   puts "machine running"
 end
